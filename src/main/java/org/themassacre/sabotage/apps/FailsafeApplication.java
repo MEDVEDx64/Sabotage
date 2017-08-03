84ebfa2ac2ec9ef68c393d559466eedfdb027d53
Andrey Bobkov <medvedx64@gmail.com>
2017-08-03 10:21:32 +0300

    FailsafeApplication complete (need testing)

--- src/main/java/org/themassacre/sabotage/apps/FailsafeApplication.java
+++ src/main/java/org/themassacre/sabotage/apps/FailsafeApplication.java
@@ -1,15 +1,45 @@
 package org.themassacre.sabotage.apps;
 
-public class FailsafeApplication extends Thread implements Application {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.themassacre.sabotage.Launcher;
 
+public abstract class FailsafeApplication extends Thread implements Application {
+	final static Logger logger = LogManager.getLogger(Launcher.class);
+	
 	@Override
 	public boolean up() {
-		// TODO Auto-generated method stub
-		return false;
+		start();
+		return true;
 	}
 
 	@Override
 	public void down() {
-		// TODO Auto-generated method stub
+		interrupt();
+	}
+	
+	protected abstract void executePayload() throws InterruptedException;
+	
+	@Override
+	public final void run() {
+		while(true) {
+			if(interrupted()) {
+				logger.info("Failsafe application has been interrupted");
+				break;
+			}
+			
+			try {
+				executePayload();
+			} catch(InterruptedException eInt) {
+			}
+			
+			catch(Throwable e) {
+				logger.error("Failsafe application has encountered an error: " + e);
+				try {
+					sleep(1000);
+				} catch (InterruptedException eInt) {
+				}
+			}
+		}
 	}
 }
